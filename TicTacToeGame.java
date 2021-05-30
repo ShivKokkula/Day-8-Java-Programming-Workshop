bb781e28a26d0898fa727c790b92da0dabbf3d02
ShivKokkula <shivbk987@gmail.com>
2021-05-30 18:58:52 +0530

    Refactor - added board method in the UC-1-Tic-Tac-Toe-Board branch

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,6 +4,17 @@ public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe Game !!!");
+		board();
+	}
+	
+	public static void board() {
+		
+		char board[] = new char[10];
+		
+		for(int i=0; i<10; i++) {
+			board[i] = ' ';
+	     }
+		
 	}
 
 }
