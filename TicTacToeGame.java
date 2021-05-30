5efbbd87e53371c25fe8d3e726e168be7aa53ff4
ShivKokkula <shivbk987@gmail.com>
2021-05-30 19:11:10 +0530

    Refactor - added ability to select X or O to user in UC-2-Select-User-Choice

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,10 +1,17 @@
 package tictactoegame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	
+	static Scanner userInput = new Scanner(System.in);
+	static char userChoice;
+	static char compChoice;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe Game !!!");
 		board();
+		selectUserChoice();
 	}
 	
 	public static void board() {
@@ -15,6 +22,18 @@ public class TicTacToeGame {
 			board[i] = ' ';
 	     }
 		
+	}
+	
+	public static void selectUserChoice() {
+		System.out.println("Please choose from X or O");
+		userChoice = userInput.next().charAt(0);
+
+		if (userChoice == 'X')
+			compChoice = 'O';
+		else
+			compChoice = 'X';
+		
+		System.out.println("User is: " + userChoice + " computer is " + compChoice);
 	}
 
 }
