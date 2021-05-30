262d4c74cb02cc7988e1d426a5aa3cea3a5ac0aa
ShivKokkula <shivbk987@gmail.com>
2021-05-30 21:49:28 +0530

    Refactor - added select position method in the UC-4-Selection-Position branch

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,28 +3,25 @@ package tictactoegame;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	
+
 	static Scanner userInput = new Scanner(System.in);
 	static char userChoice;
 	static char compChoice;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe Game !!!");
-		board();
-		selectUserChoice();
-	}
-	
-	public static void board() {
-		
-		char[][] board = {{' ','|',' ','|',' '},
-					{'-','+','-','+','-'},
-					{' ','|',' ','|',' '},
-					{'-','+','-','+','-'},
-					{' ','|',' ','|',' '}};
+		char[][] board = { { ' ', '|', ' ', '|', ' ' }, 
+						   { '-', '+', '-', '+', '-' }, 
+						   { ' ', '|', ' ', '|', ' ' },
+						   { '-', '+', '-', '+', '-' }, 
+						   { ' ', '|', ' ', '|', ' ' } };
 		showBoard(board);
-		
+		selectUserChoice();
+		selectPosition(board);
+		showBoard(board);
+
 	}
-	
+
 	public static void showBoard(char[][] board) {
 		for (char[] row : board) {
 			for (char column : row) {
@@ -33,8 +30,41 @@ public class TicTacToeGame {
 			System.out.println();
 		}
 	}
-	
-	
+
+	public static void selectPosition(char[][] board) {
+		System.out.println("Enter your placement(0-9):");
+		int pos = userInput.nextInt();
+		switch (pos) {
+		case 1:
+			board[0][0] = 'X';
+			break;
+		case 2:
+			board[0][2] = 'X';
+			break;
+		case 3:
+			board[0][4] = 'X';
+			break;
+		case 4:
+			board[2][0] = 'X';
+			break;
+		case 5:
+			board[2][2] = 'X';
+			break;
+		case 6:
+			board[2][4] = 'X';
+			break;
+		case 7:
+			board[4][0] = 'X';
+			break;
+		case 8:
+			board[4][2] = 'X';
+			break;
+		case 9:
+			board[4][4] = 'X';
+			break;
+		}
+	}
+
 	public static void selectUserChoice() {
 		System.out.println("Please choose from X or O");
 		userChoice = userInput.next().charAt(0);
@@ -43,7 +73,7 @@ public class TicTacToeGame {
 			compChoice = 'O';
 		else
 			compChoice = 'X';
-		
+
 		System.out.println("User is: " + userChoice + " computer is " + compChoice);
 	}
 
